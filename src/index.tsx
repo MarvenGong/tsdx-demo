55acba6fa86ffea69d64b429cd61f587c45deeff
marvengong <marvengong@tencent.com>
2023-11-23 19:21:39 +0800

    feat: 初始化

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import './style.less';
+
+// Delete me
+const Thing = () => {
+  return <div className="box">
+    测试react组件
+    <h1>我是标题</h1>
+  </div>;
+};
+export default Thing;
